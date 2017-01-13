b322f3155f9a6ebb41d1b3ffeff97872671641e4
RyanJing <RyanJing@users.noreply.github.com>
2017-01-13 10:55:16 -0500

    Created Vehicular Chassis to Constrain Wheels
    Hi Abraham and Jason, thought of a quick fix to constrain the bodies. I built a chassis "wall" to cage the spherical wheels and let physics engine do the rest. You may have to manually put in the ground plane again to try the scene as I am trying to reduce the number of files to upload. We can discuss more later.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player2;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - player2.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		transform.position = player2.transform.position + offset;
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start () 
+	{
+
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+
+	void FixedUpdate ()
+	{
+		//float moveHorizontal = Input.GetAxis ("Horizontal2");
+		float moveVertical = Input.GetAxis ("Vertical2");
+
+		Vector3 movement = new Vector3 (0.0f , 0.0f , moveVertical);
+
+		rb.AddForce (movement * speed); 
+	}
+}
